b7890641895fb9bf48a3305653749131f2760fdf
Hua Son Tung <tunghuason@gmail.com>
2021-08-16 15:52:25 +0700

    Customize Device Button

--- Lamp Product/Controllers/HomeViewController.swift
+++ Lamp Product/Controllers/HomeViewController.swift
@@ -12,12 +12,46 @@ class HomeViewController: UIViewController {
     @IBOutlet var leftBarButton: UIButton!
     @IBOutlet var rightBarButton: UIButton!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var tvButton: UIButton!
+    @IBOutlet var laptopButton: UIButton!
+    @IBOutlet var iphoneButton: UIButton!
 
+    @IBAction func tvClicked(_ sender: Any) {
+        if tvButton.backgroundColor == UIColor(named: "Dark gray") {
+            tvButton.backgroundColor = UIColor(named: "Light green")
+        } else {
+            tvButton.backgroundColor = UIColor(named: "Dark gray")
+        }
+        
+    }
+    
+    @IBAction func laptopClicked(_ sender: Any) {
+        if laptopButton.backgroundColor == UIColor(named: "Dark gray") {
+            laptopButton.backgroundColor = UIColor(named: "Light green")
+        } else {
+            laptopButton.backgroundColor = UIColor(named: "Dark gray")
+        }
+        
+    }
+    
+    @IBAction func iphoneClicked(_ sender: Any) {
+        if iphoneButton.backgroundColor == UIColor(named: "Dark gray") {
+            iphoneButton.backgroundColor = UIColor(named: "Light green")
+        } else {
+            iphoneButton.backgroundColor = UIColor(named: "Dark gray")
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupNavigationBarItems()
+        setupDevicesButtons()
+        
         
         
     }
@@ -30,8 +64,12 @@ class HomeViewController: UIViewController {
         
         rightBarButton.navBarButtonDesign(imageName: "bag.fill")
         
-        
-        
+    }
+    
+    private func setupDevicesButtons() {
+        tvButton.deviceButton()
+        laptopButton.deviceButton()
+        iphoneButton.deviceButton()
     }
 
 
